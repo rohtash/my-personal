0f8d0a17856f4e9e4cd36eeafa3175d911825e32
rohtash <rohtashpoonia644@gmail.com>
2015-01-19 12:16:22 +0000

    sasa

---
+++ MyApp/app/src/main/java/impl/poonia/com/myapp/L.java
@@ -0,0 +1,12 @@
+package impl.poonia.com.myapp;
+
+import android.util.Log;
+
+/**
+ * Created by Windows on 13-01-2015.
+ */
+public class L {
+    public static void m(String message){
+        Log.d("VIVZ",""+message);
+    }
+}
--- MyApp/app/src/main/java/impl/poonia/com/myapp/MainActivity.java
+++ MyApp/app/src/main/java/impl/poonia/com/myapp/MainActivity.java
@@ -1,17 +1,30 @@
 package impl.poonia.com.myapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout), toolbar);
     }
 
 
@@ -31,7 +44,11 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(this, "Hey you just hit " + item.getTitle(), Toast.LENGTH_SHORT).show();
             return true;
+        }
+        if (id == R.id.navigate) {
+            startActivity(new Intent(this, SubActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
